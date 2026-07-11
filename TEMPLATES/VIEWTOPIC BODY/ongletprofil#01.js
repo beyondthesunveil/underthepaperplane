fc0371fb292d539ee7dbb6b06cebf883422c0393
wisp. <87560864+thewanderingwhispers@users.noreply.github.com>
2026-07-11 15:07:42 +0200

    Create ongletprofil#01.js

---
+++ TEMPLATES/VIEWTOPIC BODY/ongletprofil#01.js
@@ -0,0 +1,27 @@
+jQuery(function ($) {
+
+    $('.utppVB_postprofile .post').each(function () {
+
+        var $post = $(this);
+
+        $post.find('.lithium-vb_charafield').each(function () {
+
+            var $field = $(this);
+
+            var label = $.trim(
+                $field.find('.utppVB_charalabel .label span:first').text()
+            );
+
+            if (
+                label === "Feat" ||
+                label === "Date d'inscription" ||
+				label === "Messages"
+            ) {
+                $post.find('.other').append($field);
+            }
+
+        });
+
+    });
+
+});
